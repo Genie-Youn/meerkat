bbd24488f96482b4b8ffdc1baf01960ba341814d
Geine <bonos2@wisdo.me>
2016-10-03 02:57:11 +0900

    create UserVO

---
+++ meerkat-web/src/main/java/com/genie/meerkat/vo/User.java
@@ -0,0 +1,71 @@
+package com.genie.meerkat.vo;
+
+/**
+ * @author bonos2
+ *
+ */
+public class User {
+	
+	private String number;
+	private String userId;
+	private String name;
+	private String phoneNumber;
+	private String role;
+	
+	public User(){
+	}
+	
+	public User(String userId, String name, String phoneNumber, String role){
+		this.userId = userId;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.role = role;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [number=" + number + ", userId=" + userId + ", name=" + name + ", phoneNumber=" + phoneNumber
+				+ ", role=" + role + "]";
+	}
+
+}
